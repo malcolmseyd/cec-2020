f9c0e5ac6131b45dfdd36b3aaa96b1f37a6f13ef
dereklowlind <dereklowlind@gmail.com>
2020-03-07 08:53:57 -0800

    added bar ghart code

---
+++ barTest2.py
@@ -0,0 +1,30 @@
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.cm as cm 
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection = "3d")
+
+ax.set_xlabel("x")
+ax.set_ylabel("y") 
+ax.set_zlabel("z")
+ax.set_xlim3d(0,10)
+ax.set_ylim3d(0,10) 
+ax.set_zlim3d(0,10) 
+
+alpha_base=0.5
+
+xpos = [1,1,2,2,1,1,2,3] #x coordinates of each bar
+ypos = [1,2,1,2,1,2,1,2] #y coordinates of each bar
+zpos = [0,0,0,0,1,1,1,1] #z coordinates of each bar
+colors = [(1, 0, 0, alpha_base), 'r', 'g', 'g', 'b','b','y','y']
+
+dx = np.ones(8) #width of each bar
+dy = np.ones(8) #depth of each bar
+dz = np.ones(8) #height of each bar
+
+ax.bar3d(xpos, ypos, zpos, dx, dy, dz, color=colors)
+
+plt.gca().invert_xaxis()
+plt.show()
